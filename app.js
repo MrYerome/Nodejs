6e32aea78435b4529ea6ff2c45549b4bcc3cc99d
Christopher <Christo.tpe@gmail.com>
2019-06-19 15:01:09 +0200

    room and rename

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(server);
 server.listen(8888);
 app.use(express.static(__dirname + '/public')).get('/', function (req, res) {
 
-    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Ty pe', 'text/plain');
     res.render("index.html");
 });
 
@@ -14,3 +14,26 @@ io.on('connection', () => {
     console.log("Nouvelle connection Utilisateur");
 })
 
+let rooms = [];
+rooms[0] = 0;
+io.sockets.on('connection', (socket) => {
+
+    // Parcrour des room pour attributionz
+    for (let i = 0; i < rooms.length; i++) {
+        const connectedUser = rooms[i];
+        if (connectedUser < 2) {
+            socket.roomid = i;
+            socket.join(i);
+            socket.in(socket.roomid).emit('newPlayer', 'Nouveau joueur');
+            rooms[i] = connectedUser + 1;
+        }
+    }
+
+    // Cette fonction permet au joueur de se renomer et prevenir les joueurs adverses;
+    socket.on('rename', (pseudo) => {
+        socket.pseudo = pseudo;
+        socket.in(socket.roomid).emit('rename', pseudo)
+    });
+
+
+})
